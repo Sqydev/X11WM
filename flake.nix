413d4a15489a103f67c593c3d0ca3080829c1f67
Sqyd <138494523+Sqydev@users.noreply.github.com>
2026-04-17 20:20:52 +0200

    idk

--- flake.nix
+++ flake.nix
@@ -9,19 +9,43 @@
     pkgs = nixpkgs.legacyPackages.${system};
 
     vtwm = pkgs.stdenv.mkDerivation {
-      name = "vtwm";
+      pname = "vtwm";
+      version = "0.1.0"; # Good practice to include a version
       src = ./.;
 
-      buildPhase = ''
-        make
+      # If your Makefile uses a variable for the session path, override it here.
+      # If it's hardcoded, we use postPatch to fix it globally.
+      postPatch = ''
+        substituteInPlace makefile \
+          --replace "/usr" "$out"
       '';
 
+      # Using makeFlags is cleaner than overriding phases manually
+      makeFlags = [ "PREFIX=$(out)" ];
+
+      # Note: buildPhase and installPhase are usually handled automatically 
+      # by stdenv if you have a standard Makefile. 
+      # Only override them if your Makefile requires non-standard arguments.
       installPhase = ''
+        runHook preInstall
         make install PREFIX=$out
+        runHook postInstall
       '';
+
+      nativeBuildInputs = with pkgs; [
+        # Add any build tools here (e.g., pkg-config)
+      ];
+
+      buildInputs = with pkgs; [
+        xorg.libX11
+        xorg.libXinerama
+        # Add other X11 dependencies here
+      ];
     };
   in {
-    packages.${system}.vtwm = vtwm;
-    packages.${system}.default = vtwm;
+    packages.${system} = {
+      vtwm = vtwm;
+      default = vtwm;
+    };
   };
 }
