452ee212792364f2b3d353c12f1e383a6b56e739
Sqyd <138494523+Sqydev@users.noreply.github.com>
2026-04-17 22:27:52 +0200

    Works?

--- flake.nix
+++ flake.nix
@@ -1,5 +1,15 @@
 {
   description = "vtwm nixos installer";
+  # NOTE: for the lack of better option. to upgrade type:
+  # sudo nixos-rebuild switch --option tarball-ttl 0
+  #    To install on system add to your config.nix:
+  # let
+  # 	vtwm = builtins.getFlake "git+https://github.com/Sqydev/vtwm.git";
+  # in
+  #   And in environment.systemPackages = with pkgs; [
+  # vtwm.packages.${pkgs.system}.default
+  # And like enywhere:
+  # services.vtwm.enable = true;
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
@@ -8,34 +18,39 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
 
-    vtwm = pkgs.stdenv.mkDerivation {
+    vtwmPkg = pkgs.stdenv.mkDerivation {
       pname = "vtwm";
       version = "0.1.0";
       src = ./.;
 
-      buildInputs = with pkgs; [
-        libX11
-        libXinerama
-      ];
+      buildInputs = [ pkgs.libX11 pkgs.libXinerama ];
+      nativeBuildInputs = [ pkgs.pkg-config pkgs.gnumake ];
 
-      nativeBuildInputs = with pkgs; [
-        pkg-config
-        gnumake
-      ];
+      buildPhase = "make build";
 
-      buildPhase = ''
-        make build
+      installPhase = ''
+        make install PREFIX=$out DESTDIR=
       '';
-
-		installPhase = ''
-		  make install PREFIX=$out DESTDIR=
-		  mkdir -p $out/share/xsessions
-		  substitute vtwm.desktop $out/share/xsessions/vtwm.desktop \
-		    --replace "Exec=vtwm" "Exec=$out/bin/vtwm"
-		'';
     };
   in
   {
-    packages.${system}.default = vtwm;
+    packages.${system}.default = vtwmPkg;
+
+    nixosModules.default = { config, lib, pkgs, ... }: {
+      options.services.vtwm.enable = lib.mkEnableOption "vtwm window manager";
+
+      config = lib.mkIf config.services.vtwm.enable {
+        services.xserver.enable = true;
+
+        services.xserver.displayManager.session = [
+          {
+            name = "vtwm";
+            manage = "window";
+            start = "exec ${vtwmPkg}/bin/vtwm";
+          }
+        ];
+        environment.systemPackages = [ vtwmPkg ];
+      };
+    };
   };
 }
